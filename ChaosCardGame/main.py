332f3160f8cf38a9667fd561e3cfe85cd96c5e7a
SchoolProject12345 <143693429+SchoolProject12345@users.noreply.github.com>
2023-11-02 12:51:10 +0100

    style: updated comments & removed useless code.

--- ChaosCardGame/main.py
+++ ChaosCardGame/main.py
@@ -1,7 +1,7 @@
 # The Card Game
 
-from dataclasses import dataclass # easier Class declaration
-from enum import IntEnum # for clear, lightweight (Int) elements/state.
+from dataclasses import dataclass # easier class declaration
+from enum import IntEnum # for clear, lightweight (int) elements/state.
 from numpy import random as rng # for shuffle function/rng effects
 
 def getcards() -> dict: # only used to define `global CARDS`
@@ -10,7 +10,7 @@ def getcards() -> dict: # only used to define `global CARDS`
     io.close();
     id = -1 # starts at -1 + 1 = 0
     return [AbstractCard.from_json(card, (id := id + 1)) for card in json if not "example" in card] # please note that whether example is put to true or false it is excluded from the list.
-CARDS     = getcards();
+CARDS = getcards();
 def DEV(): return True; # enable debugging; function to avoid taking from global scope
 
  # convenience functions, might be moved to a separate file later.
@@ -43,7 +43,7 @@ def warn(*args, dev = DEV(), **kwargs):
     do something here
     ```
     """
-    if dev: # hard check to avoid mistakes (and to avoid taking DEV from global scope, which would hugely slow down the code)
+    if dev: # hard check to avoid mistakes
         print("\x1b[1;33m┌ Warning:\n└ ", *args, "\x1b[0m", **kwargs) # might not work in every terminal, but should in VS Code
     return True # this is definitevely not spaghetti code.
 def ifelse(cond: Bool, a, b):
@@ -53,25 +53,19 @@ def ifelse(cond: Bool, a, b):
     return b
 
 class Element(IntEnum):
-    elementless = 0 # used instead of None as a placeholder; shouldn't be used otherwise (except if that's intentional?).
-    rock = 1 # I added it we never know. (Technically you could replace it by earth)
-    paper = 2
-    scissor = 3
-    water = 4
-    fire = 5
-    air = 6
-    earth = 7
-    chaos = 8 # Weak defense against all  (x1,2 damage taken) but powerful attack against all (x1.2 damage output)
+    elementless = 0 # used instead of None as a placeholder (for type-safeness) or for elementless card types for flexibility when using Element.effective
+    water = 1
+    fire = 2
+    air = 3
+    earth = 4
+    chaos = 5 # Weak defense against all but chaos (x1,2 damage taken) but powerful attack against all but chaos (x1.2 damage output)
     def from_str(name: str):
-        match name.lower():
-            case "rock": return Element.rock
-            case "paper": return Element.paper
-            case "scissor": return Element.scissor
+        match name.strip().lower():
             case "water": return Element.water
             case "fire": return Element.fire
             case "air": return Element.air
             case "earth": return Element.earth
-            case "chaos": return Element.chaos
+            case "chaos": return Element.chaos # keeping chaos as it is a more interesting alternative than elementless for cards with no obvious element
             case _: return warn(f"Tried to form an Element from \"{name}\"; returned Element.elementless instead.") and Element.elementless
     def from_json(json: dict):
         if "element" not in json:
@@ -92,14 +86,9 @@ class Element(IntEnum):
         False
         ```
         """
+        if self == Element.chaos and other == Element.chaos:
+            return False # changed chaos to resist chaos to avoid chaos being just a x1.2 in power and /1.2 in HP
         if self == Element.chaos or other == Element.chaos:
-            return True
-
-        if self == Element.rock and other == Element.scissor:
-            return True
-        if self == Element.paper and other == Element.rock:
-            return True
-        if self == Element.scissor and other == Element.paper:
             return True
 
         if self == Element.water and other == Element.fire:
@@ -125,24 +114,25 @@ class Element(IntEnum):
         True
         ```
         """
+        if self == Element.chaos and other == Element.chaos:
+            return True
         if self == Element.chaos or other == Element.chaos:
             return False
         return other.effectiveness(self)
 class State(IntEnum):
     default = 0 # placeholder
-    defeated = 1 # I suppose
 
 @dataclass
 class Attack:
     power: int
     def from_json(json: dict):
-        pass # TODO: return an object
+        pass # TODO: return an Attack object
 
 @dataclass
 class AbstractCard:
     name: str
     id: int
-    element: Element # all card types until now have an element; if a new card type doesn't require an element, we'll force the element field to Element.elementless in the from_json constructor (which allows flexibility with Element.efectiveness).
+    element: Element # all card types until now have an element; if a new card type doesn't require an element, we'll force the element field to Element.elementless in the from_json constructor (which allows flexibility with Element.efective).
     def __init__(self, *args):
         warn("AbstractCard class serves only as a superclass; initialize object of more specific classes instead.")
     def from_json(json: dict, id: int): # id from index in list
@@ -155,9 +145,10 @@ class AbstractCard:
         if type == "spell":
             return SpellCard.from_json(json, id)
         warn(f"Card with name {json['name']} has type {type} which isn't handled.")
-    def copy(self): # Method is inherited to every subclass.
-        return type(self)(**vars(self)) # a bit spaghettit coded but I can't really do better because Python.
-            
+    def copy(self): # Method is inherited to every subclass. Will be needed as otherwise every card with the same id have shared HP.
+        return type(self)(**vars(self)) # a bit spaghettit coded but I can't really do better because Python. TODO: copy recursively to avoid sharing completely
+    def iscommander(self): return False # in case we can't know if the card is a Creature or not, it avoids a crash.
+
 @dataclass
 class CreatureCard(AbstractCard):
     hp: int
@@ -172,18 +163,17 @@ class CreatureCard(AbstractCard):
         )
         if getordef(json, "commander", False): # so we don't need to define "commander":false for every card (might be changed later to "type":"commander" though).
             return CommanderCard(*args)
-        return CreatureCard(*args) # use default argument for State (it means all CreatureCard share the same state object due to Python being Python)
+        return CreatureCard(*args) # use default argument for State
     def damage(self, d: int):
         "Reduce hp by d but never goes into negative; exists so we can add modifier in it if necessary."
         if DEV() and type(d) != int:
-            warn(f"Card with name \"{self.name}\" took non integer damages; converting value to int. /!\\ PLEASE FIX CONVERSION IS DISABLED WHEN OUT OF DEV MODE /!\\")
+            warn(f"Card with name \"{self.name}\" took non integer damages; converting value to int. /!\\ PLEASE FIX: automatic type conversion is disabled when out of DEV mode /!\\")
             d = int(d)
         if d > self.hp
             return (self.hp := 0)
         return (self.hp := self.hp - d)
     def attack(self, attack: Attack, other) # other is creature card.
         return other.damage(attack.power * ifelse(self.element.effective(other.element), 12, 10) // ifelse(other.element.resist(self.element), 12, 10))
-    def iscommander(self): return False
 @dataclass
 class CommanderCard(CreatureCard):
     # Note: all field of CommanderCard must have a default value as CreatureCard ends with one. (inheritance)
