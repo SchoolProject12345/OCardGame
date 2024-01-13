ba1204afbca68b8609ee838d82576f215fb26797
SchoolProject12345 <143693429+SchoolProject12345@users.noreply.github.com>
2024-01-13 08:42:16 +0100

    Perf; features; style: store loaded_data in RAM; allow to use different handler/to listen on the same thread; new function `join_connection` for easier dispatch at compile-time.

--- ChaosCardGame/Network/network.py
+++ ChaosCardGame/Network/network.py
@@ -1,10 +1,18 @@
 import socket
 import json
 import threading
-import shutil
 
+def get_data(loaded_data = {}):
+    if bool(loaded_data):
+        return loaded_data
+    with open('template.json') as file:
+        try:
+            loaded_data.update(json.load(file))
+        finally:
+            file.close()
+    return loaded_data
 
-def data_handler(action: str, path: str, data: str):
+def data_handler(action: str, path: str, data: str, loaded_data: dict = get_data()):
     """
     Appends/removes/replaces data in a JSON file.
 
@@ -25,9 +33,6 @@ def data_handler(action: str, path: str, data: str):
             data = json.loads(data)
     except:
         pass
-
-    with open('./public/data.json', 'r') as file:
-        loaded_data = json.load(file)
 
     keys = path.split('/')
     # Navigate the JSON structure based on the path
@@ -77,15 +82,11 @@ def data_handler(action: str, path: str, data: str):
         print("Invalid action. Supported actions: 'append', 'remove', 'replace'")
         return
 
-    # Write the modified data back to the file
-    with open("./public/data.json", 'w') as file:
-        json.dump(loaded_data, file, indent=2)
-
+    # can be read from here, but shouldn't get modified as the modifiction wouldn't be send to the peer.
+    return loaded_data
 
 def get_ip():
-    """
-    get local IP (its clapped, but works)
-    """
+    "get local IP (its clapped, but works)"
     clapped = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     clapped.connect(("8.8.8.8", 80))
     host = clapped.getsockname()[0]
@@ -95,37 +96,40 @@ def get_ip():
 
 def send(client_socket: socket.socket, action: str, path: str, data: str):
     """
-    send() updates both the local and peer's JSON file.
+    `send` updates both the local and peer's JSON file.
+    See `data_handler` for information on arguments.
     """
     # Update local JSON
     data_handler(action, path, data)
     # Send update to peer
     client_socket.send((f"{action}|{path}|{data}").encode("utf-8"))
 
+def default_handler(data: str) -> bool:
+    "Default function used by `listen`."
+    if not bool(data):
+        return False
+    data = data.split("|")
+    action = data[0]
+    path = data[1]
+    data = data[2]
+    data_handler(action, path, data)
+    return True
 
-def listen(client_socket: socket.socket):
+def listen(client_socket: socket.socket, handler = default_handler):
     """
-    listen() must run constantly on a separate
+    `listen(client_socket)` must run constantly on a separate
     thread, as it needs to listen all the time
     for changes to the public directory
     """
     while True:
-        data = client_socket.recv(1024).decode("utf-8")  # 1 kb of data
-
-        if not data:  # if empty byte string (socket was closed)
+        data = client_socket.recv(1024).decode("utf-8") # 1 kb of data
+        if not (default_handler(data) or bool(data)): # if empty byte string (socket was closed)
             client_socket.close()
             break
 
-        data = data.split("|")
-        action = data[0]
-        path = data[1]
-        data = data[2]
-        data_handler(action, path, data)
-
-
 ## ======================= SET UP CONNECTION ======================= ##
 
-def listen_for_connection(ip: str, port: str):
+def listen_for_connection(ip: str, port: int):
     """
     This function is run by the host while
     the host is waiting for a connection request.
@@ -139,43 +143,39 @@ def listen_for_connection(ip: str, port: str):
     client_socket, addr = listening_socket.accept()  # one connection max
     print(f"Accepted connection from {addr}")
 
-    network_thread = threading.Thread(target=listen, args=(client_socket,))
-    network_thread.start()
-
     return client_socket
 
+def join_connection(target_ip: str, port: int):
+    "Allow to join a connection started by `listen_for_connection` or `start_peer_to_peer` without running `listen` on separated thread."
+    client_socket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)  # sock_stream for TCP
+    client_socket.settimeout(5)
 
-def start_peer_to_peer(action, target_ip: str = ""):
+    try:
+        client_socket.connect((target_ip, port))
+        client_socket.settimeout(None)
+    except socket.error as e:
+        print(f"Error connecting to {target_ip}:{port}: {str(e)}")
+        return
+
+def start_peer_to_peer(action, target_ip: str = "", port: int = 12345, handler = default_handler): # You can choose any available port
     """
-    start_peer_to_peer() is the starting point of the network program.
+    `start_peer_to_peer(action, target_ip)` is the starting point of the network program.
     The user will choose to join or to host a party.
     """
-    ip = get_ip()
-
     # reset the data.json file
-    shutil.copyfile('template.json', './public/data.json')
-    # host = socket.gethostname()
-    port = 12345  # You can choose any available port
-    print("IP: ", ip)
+    get_data().clear()
 
     if action == "start":
+        ip = get_ip()
+        print("IP:", ip)
         client_socket = listen_for_connection(ip, port)
-        return client_socket
     else:
-        target_port = 12345
-
-        client_socket = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)  # sock_stream for TCP
-        client_socket.settimeout(5)
-
-        try:
-            client_socket.connect((target_ip, target_port))
-            client_socket.settimeout(None)
-        except socket.error as e:
-            print(f"Error connecting to {target_ip}:{target_port}: {str(e)}")
+        client_socket = join_connection(target_ip, port)
+        if client_socket is None:
             return False
 
         # This will run separately from the game.
-        network_thread = threading.Thread(target=listen, args=(client_socket,))
-        network_thread.start()
-        return client_socket
+    network_thread = threading.Thread(target=listen, args=(client_socket, handler))
+    network_thread.start()
+    return client_socket
