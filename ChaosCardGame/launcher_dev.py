74c3a1e74512979d152a5b47008eabffe8d64fc2
SchoolProject12345 <143693429+SchoolProject12345@users.noreply.github.com>
2024-01-18 07:04:17 +0100

    Load cards before trying to host/join.
    Avoid connection reset due to wrong parsing.

--- ChaosCardGame/launcher_dev.py
+++ ChaosCardGame/launcher_dev.py
@@ -1,10 +1,11 @@
 from Network import server
 
+server.core.getCARDS()
 
 match server.core.cleanstr(input("Would you like to `host` or `join` a party? ")):
     case "host": server.host(
         input("Choose your username: "),
-        server.core.ifelse(server.core.cleanstr("Would you like to localhost [yes/no]? ") == "yes",
+        server.core.ifelse(server.core.cleanstr(input("Would you like to localhost [yes/no]? ")) == "yes",
             "127.0.0.1",
             server.net.get_ip()
         )
