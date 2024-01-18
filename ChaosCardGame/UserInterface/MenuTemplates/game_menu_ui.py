86d7a1fef41df521696e45abfd126474eaba11b9
AxaQuilPre <tablemaker99@gmail.com>
2024-01-18 09:55:05 +0100

    Added revert state.

--- ChaosCardGame/UserInterface/MenuTemplates/game_menu_ui.py
+++ ChaosCardGame/UserInterface/MenuTemplates/game_menu_ui.py
@@ -1,3 +1,4 @@
+import pygame
 from UserInterface.OCG_Vision.vision_main import State, ImageButton
 from Assets.menu_assets import MenuBackgrounds, MenuButtons, alpha_converter
 from UserInterface.ui_settings import SCREEN_CENTER
@@ -5,7 +6,7 @@ from UserInterface.ui_settings import SCREEN_CENTER
 class GameMenu(State):
     def __init__(self,screen):
         self.screen = screen
-        self.is_anchor = True
+        self.is_anchor = False
         self.local_options = ["GameMenu"]
         super().__init__(self.screen, self.is_anchor, self.local_options)
 
@@ -14,6 +15,10 @@ class GameMenu(State):
 
     def game_menu(self):
         self.screen.blit(self.bg_game_menu_image, self.bg_game_menu_rect)
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.revert_state()
 
     def state_manager_hook(self):
         if self.local_state == self.local_options[0]:
