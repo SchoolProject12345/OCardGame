692459597447e5ca638e7664ded8c995ed468b7b
AxaQuilPre <tablemaker99@gmail.com>
2024-01-19 23:27:37 +0100

    Implemented timed pause screen toggle. Suboptimal

--- ChaosCardGame/UserInterface/MenuTemplates/game_menu_ui.py
+++ ChaosCardGame/UserInterface/MenuTemplates/game_menu_ui.py
@@ -1,4 +1,5 @@
 import pygame
+from time import time
 from UserInterface.OCG_Vision.vision_main import State, ImageButton
 from Assets.menu_assets import MenuBackgrounds, MenuButtons, alpha_converter
 from UserInterface.ui_settings import SCREEN_CENTER
@@ -15,15 +16,21 @@ class GameMenu(State):
         self.bg_game_menu_rect = self.bg_game_menu_image.get_rect()
 
         self.bg_pause_menu_image = MenuBackgrounds.bg_pause_menu_image.convert_alpha()
-        self.bg_pause_menu_rect = self.bg_pause_menu_image.get_rect(center=SCREEN_CENTER)
-        
+        self.bg_pause_menu_rect = self.bg_pause_menu_image.get_rect(
+            center=SCREEN_CENTER)
+
+        self.is_paused = False
+        self.last_pause_toggle = -1
 
     def game_menu(self):
-        self.screen.blit(self.bg_game_menu_image,self.bg_game_menu_rect)
+        self.screen.blit(self.bg_game_menu_image, self.bg_game_menu_rect)
 
-        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
-            self.screen.blit(self.bg_pause_menu_image,self.bg_pause_menu_rect)
-            
+        if pygame.key.get_pressed()[pygame.K_ESCAPE] and time()-self.last_pause_toggle > 0.25:
+            self.is_paused = not self.is_paused
+            self.last_pause_toggle = time()
+
+        if self.is_paused:
+            self.screen.blit(self.bg_pause_menu_image, self.bg_pause_menu_rect)
 
     def state_manager_hook(self):
         if self.local_state == self.local_options[0]:
