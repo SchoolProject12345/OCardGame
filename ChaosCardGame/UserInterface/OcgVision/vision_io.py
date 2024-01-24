999b5a1239969b5fcfc951e864033e6aa0ee4834
AxaQuilPre <tablemaker99@gmail.com>
2024-01-24 15:42:23 +0100

    Implemented SingleExec key toggle

--- ChaosCardGame/UserInterface/OcgVision/vision_io.py
+++ ChaosCardGame/UserInterface/OcgVision/vision_io.py
@@ -19,7 +19,32 @@ class KeyToggle(SingleExecKey):
     def __init__(self, target_key, init_val: bool):
         super().__init__(target_key)
         self.init_val = init_val
-    
+
+    def toggle(self):
+        self.init_val = not self.init_val
+
+    def update(self, keys):
+        if self.handle_key(keys):
+            self.toggle()
+            return self.init_val
+        else:
+            return self.init_val
+
+
+class KeyToggleOnRelease(KeyToggle):
+    def __init__(self, target_key, init_val: bool):
+        super().__init__(target_key, init_val)
+
+    def handle_key(self, keys):
+        if keys[self.target_key] and not self.key_blocked:
+            self.key_blocked = True
+            return False
+        elif not keys[self.target_key] and self.key_blocked:
+            return True
+        else:
+            self.key_blocked = False
+            return False
+
     def toggle(self):
         self.init_val = not self.init_val
 
