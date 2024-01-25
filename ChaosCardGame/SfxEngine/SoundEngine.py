1646ff7a4087b354b730d15d4d35add86503c441
Erdaberda <therlyingchicken23@gmail.com>
2024-01-25 09:37:56 +0100

    a

--- ChaosCardGame/SfxEngine/SoundEngine.py
+++ ChaosCardGame/SfxEngine/SoundEngine.py
@@ -3,16 +3,21 @@ from pygame import mixer, mixer_music
 import pygame
 from pygame.locals import *
 
+import os
+
 pygame.init()
 
 devmode = True
+
+sound_file_path = os.path.join(os.getcwd(), 'sfxengine', 'SFX', 'ClickSound12.wav')
+
 
 if devmode:
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.mixer.init()
-    sound = pygame.mixer.Sound('path_to_your_sound_file.wav')
+    sound = pygame.mixer.Sound(sound_file_path)
     button_color = (255, 0, 0) # Red color
     button_width = 100
     button_height = 50
