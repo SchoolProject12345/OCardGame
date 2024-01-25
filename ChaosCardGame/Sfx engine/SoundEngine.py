5c27eb80438a73be5db7bc3e6f563faa6191bba4
Erdaberda <therlyingchicken23@gmail.com>
2024-01-25 08:52:44 +0100

    daw

---
+++ ChaosCardGame/Sfx engine/SoundEngine.py
@@ -0,0 +1,36 @@
+## Berda's Soung engine v0.1
+from pygame import mixer, mixer_music
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+devmode = True
+
+if devmode:
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound('path_to_your_sound_file.wav')
+    button_color = (255, 0, 0) # Red color
+    button_width = 100
+    button_height = 50
+    button_x = (WINDOW_WIDTH - button_width) // 2
+    button_y = (WINDOW_HEIGHT - button_height) // 2
+    button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if button_rect.collidepoint(mouse_pos):
+                    sound.play()
+
+        window.fill((0, 0, 0)) # Fill the window with black color
+        pygame.draw.rect(window, button_color, button_rect) # Draw the button
+        pygame.display.flip() # Update the display
+
+pygame.quit()
