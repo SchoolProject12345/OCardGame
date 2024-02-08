8b1672ccace962c37e889abd91f2c7897c834c3c
Erdaberda <therlyingchicken23@gmail.com>
2024-02-08 14:34:29 +0100

    added mute status variable because why not used in settings OIFJWEOIGHWEIOFGHEWGOIWHGIOJGIR

--- ChaosCardGame/SfxEngine/SoundEngine.py
+++ ChaosCardGame/SfxEngine/SoundEngine.py
@@ -2,6 +2,8 @@
 from pygame import mixer
 import pygame
 import os
+
+from sympy import false, true
 from utility import cwd_path
 
 
@@ -16,6 +18,7 @@ def sound_handle(track:str , action_type:str = "play", volume:int=100, channel:i
     sound.set_volume(volume/100)
 
     previous_volume = volume/100
+    mute_status = False
 
     if action_type == "play":
         sfxchannel.play(sound, loops=-1 if loop else 0)
@@ -28,12 +31,16 @@ def sound_handle(track:str , action_type:str = "play", volume:int=100, channel:i
 # pour mote unmute, juste mute + channel du track (by default music deverait etre 2)
 # NE PAS UTILSER LA FONC AVEC VOLUME 0 POUR MUTE, UTILISER JUSTE ARGUMENT MUTE ET LE BON CHANNEL.
         
-    if action_type == "mute":
+    if action_type == "mute" and mute_status == False:
         previous_volume = sound.get_volume() *  100
         sound.set_volume(0)
+        mute_status = True
+    else:
+        print("mute action non permitted, check if you didnt already mute somewhere")
 
     if action_type == "unmute":
         sound.set_volume(previous_volume/100)
+        mute_status = False
 
 
 
