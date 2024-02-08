1c0b1669acd37c58ace3898f17ff7d786eae7d73
Erdaberda <therlyingchicken23@gmail.com>
2024-02-08 14:24:00 +0100

    mute mode in testing phase

--- ChaosCardGame/SfxEngine/SoundEngine.py
+++ ChaosCardGame/SfxEngine/SoundEngine.py
@@ -5,7 +5,7 @@ import os
 from utility import cwd_path
 
 
-def sound_handle(track:str="ClickSound12" , action_type:str = "play", volume:int=100, channel:int=5, loop:bool=False):
+def sound_handle(track:str , action_type:str = "play", volume:int=100, channel:int=5, loop:bool=False):
     # a terminer
 
 
@@ -15,24 +15,25 @@ def sound_handle(track:str="ClickSound12" , action_type:str = "play", volume:int
     sound = pygame.mixer.Sound(sfx_path)
     sound.set_volume(volume/100)
 
-    previous_volume = volume
-
+    previous_volume = volume/100
 
     if action_type == "play":
         sfxchannel.play(sound, loops=-1 if loop else 0)
 
     if action_type == "stop":
-        pygame.mixer.Channel(channel).stop()
+        sfxchannel.stop()
 
-    if action_type == "break":
-        sound.stop()
 
 # faut test
-    if action_type == "mute_all":
+# pour mote unmute, juste mute + channel du track (by default music deverait etre 2)
+# NE PAS UTILSER LA FONC AVEC VOLUME 0 POUR MUTE, UTILISER JUSTE ARGUMENT MUTE
+        
+    if action_type == "mute":
+        previous_volume = sound.get_volume() *  100
         sound.set_volume(0)
 
-    if action_type == "unmute_all":
-        sound.set_volume(previous_volume)
+    if action_type == "unmute":
+        sound.set_volume(previous_volume/100)
 
 
 
