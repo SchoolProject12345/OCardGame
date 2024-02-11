ce723edc09a34c5ed8a7c882bbf851d1ae948d26
Erdaberda <therlyingchicken23@gmail.com>
2024-02-11 21:56:41 +0100

    idk nothign changed

--- ChaosCardGame/SfxEngine/SoundEngine.py
+++ ChaosCardGame/SfxEngine/SoundEngine.py
@@ -8,6 +8,17 @@ from utility import cwd_path, get_setting, get_settings, static
 @static
 def sound_handle(track: str = "ClickSound12" , action_type: str = "play", volume: int = 100, channel: int = 5, loop: bool = False, is_muted: bool = False):
     # à terminer
+    """Erda's basic pygame sound engine, lets you handle sounds, music, effects etc...
+    How work? sound_handle( 
+        - what track? only put name not the extension
+        - what do you want to do with it? play stop mute/unmute
+        - at what volume ? from 0 to 100
+        - in what channel? (you cant put 2 things at the same time in the same channel, use it carefully)
+        - want it to loop? (true or false)
+        - is_muted?... this one is tricky, use it only when you mute something( in that case use only sound_handle( mute/unmute, what channel, is_muted= is muted option from settings file))
+        )
+        to be finished, it works even though its not perfect so dont touch it
+    """
 
     sfx_path = os.path.join(cwd_path, "Assets", "Sfx", str(track) + ".wav")
     
@@ -33,7 +44,7 @@ def sound_handle(track: str = "ClickSound12" , action_type: str = "play", volume
             sfxchannel.set_volume(0)
             print("tried to mute")
         else:
-            sfxchannel.set_volume(get_setting("volume", 100)/100)
+            sfxchannel.set_volume(get_setting("volume", 100))
             print("tried to unmute")
             get_settings()["volume"] = int(sfxchannel.get_volume()*100)
 
