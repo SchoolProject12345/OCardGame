af9963ff05aee91f2bd67954df72e600697bca83
SchoolProject12345 <143693429+SchoolProject12345@users.noreply.github.com>
2024-02-14 06:51:32 +0100

    Fix heal logging.

--- ChaosCardGame/Core/core_main.py
+++ ChaosCardGame/Core/core_main.py
@@ -1500,27 +1500,30 @@ class ActiveCard:
             })
             passive.execute(**kwargs_)
         return amount
+    @static
     def indirectdamage(self, amount: int) -> int:
         "Reduce HP by amount but never goes into negative, then return damage dealt."
         if self.state == State.damageless:
             return 0
-        if DEV() and type(amount) != int:
+        if DEV() and not isinstance(amount, int):
             warn(f"Card with name \"{self.card.name}\" took non integer damages; converting value to int. /!\\ PLEASE FIX: automatic type conversion is disabled when out of DEV mode /!\\")
             amount = int(amount)
         if amount > self.hp:
             amount = self.hp
-        self.hp -= amount
         if amount != 0:
+            self.hp -= amount
             self.board.logs.append(f"-damage|{self.namecode()}|{self.hp}/{self.card.max_hp}")
         return amount
+    @static
     def heal(self, amount: int) -> int:
         "Heal `self` from `amount` damage while never overhealing past max HP and return amount healed."
-        if DEV() and type(amount) != int:
+        if DEV() and not isinstance(amount, int):
             warn(f"Card with name \"{self.card.name}\" healed from non integer damages; converting value to int. /!\\ PLEASE FIX: automatic type conversion is disabled when out of DEV mode /!\\")
             amount = int(amount)
         amount = min(self.card.max_hp - self.hp, amount)
-        self.hp += amount
-        self.board.logs.append(f"-heal|{self.namecode()}|{self.hp}/{self.card.max_hp}")
+        if amount != 0:
+            self.hp += amount
+            self.board.logs.append(f"-heal|{self.namecode()}|{self.hp}/{self.card.max_hp}")
         return amount
     def endturn(self):
         "Apply all effects at the end of turn."
