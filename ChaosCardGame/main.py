6ddd3c2209e5de2d76b26de1eec0132f9edb786c
AxaQuilPre <tablemaker99@gmail.com>
2024-02-16 16:54:27 +0100

    Refactor

--- ChaosCardGame/main.py
+++ ChaosCardGame/main.py
@@ -4,6 +4,7 @@ from Debug.logger import setup_logger
 setup_logger()
 from UserInterface.ocg_app import OcgGame
 # fmt: on
+
 if __name__ == "__main__":
     try:
         logging.info("Launching app")
