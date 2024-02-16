715aff87a1b47268cc4b2a303cbd022bb6ff8c31
Erdaberda <therlyingchicken23@gmail.com>
2024-02-16 08:13:08 +0100

    grouped sound channels into litsts

--- ChaosCardGame/SfxEngine/SoundEngine.py
+++ ChaosCardGame/SfxEngine/SoundEngine.py
@@ -20,34 +20,44 @@ def sound_handle(track: str = "ClickSound12" , action_type: str = "play", volume
         to be finished, it works even though its not perfect so dont touch it
     """
 
-    sfx_path = os.path.join(cwd_path, "Assets", "Sfx", str(track) + ".wav") # put only filename, not extension in track
-    
-    sfxchannel = mixer.Channel(channel)
-    sound = mixer.Sound(sfx_path)
-    sound.set_volume(volume/100)
+    actions = ["play", "stop", "mute/unmute"]
+    if action_type not in actions:
+        raise ValueError(f"invalid action {action_type}, check spelling or whatever did you had typen in bruv")
+    else:
 
-    if action_type == "play":
-        sfxchannel.play(sound, loops= -1 if loop else 0)
+        sfx_path = os.path.join(cwd_path, "Assets", "Sfx", str(track) + ".wav") # put only filename, not extension in track
+        
+        sfxchannel = mixer.Channel(channel)
+        sound = mixer.Sound(sfx_path)
+        sound.set_volume(volume/100)
 
-    if action_type == "stop":
-        sfxchannel.stop()
+        if action_type == "play":
+            sfxchannel.play(sound, loops= -1 if loop else 0)
+
+        if action_type == "stop":
+            sfxchannel.stop()
 
 
-    # faut test
-    # pour mote unmute, juste mute + channel du track (by default music deverait etre 2)
-    # NE PAS UTILSER LA FONC AVEC VOLUME 0 POUR MUTE, UTILISER JUSTE ARGUMENT MUTE ET LE BON CHANNEL.
 
-    if action_type == "mute/unmute":
+        music_channels = [2]
+        sfx_channels = [3]
+        speech_channels = [7]
+        # ajouter par la suite tous les channels avec le so et les vruits, pour regrouper les truks a mute.
+        # faut test
+        # pour mote unmute, juste mute + channel du track (by default music deverait etre 2)
+        # NE PAS UTILSER LA FONC AVEC VOLUME 0 POUR MUTE, UTILISER JUSTE ARGUMENT MUTE ET LE BON CHANNEL.
 
-        if get_setting("mute", False):
-            get_settings()["volume"] = int(sfxchannel.get_volume()*100)
-            sfxchannel.set_volume(0)
-            print("tried to mute")
-        else:
-            sfxchannel.set_volume(get_setting("volume", 100)/100)
-            # why it say nono error here ^
-            print("tried to unmute")
-            get_settings()["volume"] = int(sfxchannel.get_volume()*100)
+        if action_type == "mute/unmute":
+
+            if get_setting("mute", False):
+                get_settings()["volume"] = int(sfxchannel.get_volume()*100)
+                sfxchannel.set_volume(0)
+                print("tried to mute")
+            else:
+                sfxchannel.set_volume(get_setting("volume", 100)/100)
+                # why it say nono error here ^
+                print("tried to unmute")
+                get_settings()["volume"] = int(sfxchannel.get_volume()*100)
 
 
 
