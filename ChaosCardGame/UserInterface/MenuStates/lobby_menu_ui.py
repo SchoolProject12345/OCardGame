53211c2c7a2b8a262eae4aa29976d64fd95d43ff
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-02-18 18:42:41 +0100

    simplified username shenanigans

--- ChaosCardGame/UserInterface/MenuStates/lobby_menu_ui.py
+++ ChaosCardGame/UserInterface/MenuStates/lobby_menu_ui.py
@@ -41,11 +41,11 @@ class LobbyMenu(State):
         self.screen.blit(self.bg_lobby_image, self.bg_lobby_rect)
 
         if self.local_is_hosting == True:
-            self.hostusername_text_content = handle.get_state()["local"]["name"]
+            self.hostusername_text_content = get_setting("username", "")
             self.username_text_content = handle.get_state()["remote"]["name"]
         elif self.local_is_hosting == False:
             self.hostusername_text = handle.get_state()["remote"]["name"]
-            self.username_text_content = handle.get_state()["local"]["name"]
+            self.username_text_content = get_setting("username", "")
 
         self.hostusername_text.render(self.hostusername_text_content)
         self.username_text.render(self.username_text_content)
