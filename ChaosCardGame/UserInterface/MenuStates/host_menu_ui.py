bf6892e37bda1348426300272086262d3f6ebc81
SchoolProject12345 <143693429+SchoolProject12345@users.noreply.github.com>
2024-02-19 17:51:38 +0100

    Add fetch_handler to retrieve handle value.

--- ChaosCardGame/UserInterface/MenuStates/host_menu_ui.py
+++ ChaosCardGame/UserInterface/MenuStates/host_menu_ui.py
@@ -1,6 +1,5 @@
 import pygame
 import os
-import Network.network as net
 import Network.server as server
 from UserInterface.OcgVision.vision_io import KeyRel
 from UserInterface.OcgVision.vision_main import State, ImageButton, SelectTextBox
@@ -57,13 +56,18 @@ class HostMenu(State):
             get_settings()["roomname"] = self.tb_roomname.text # IMPLEMENT INVALID ROOMANME
             get_settings()["is_hosting"] = False
             self.change_state("LobbyMenu")
-            net.threading.Thread(target=server.host, args=(self.hostmenu_tb_username.text, self.ipaddress), daemon=True).start()
+            server.net.threading.Thread(target=self.fetch_handler, args=(self.hostmenu_tb_username.text, self.ipaddress), daemon=True).start()
 
         if self.hostmenu_exit_button.answer() or self.escp_rel.update(pygame.event.get(pygame.KEYUP)):
             get_settings()["username"] = self.hostmenu_tb_username.text # IMPLEMENT INVALID USERNAME
             get_settings()["roomname"] = self.tb_roomname.text # IMPLEMENT INVALID ROOMANME
             self.revert_state()
 
+    def fetch_handler(self, username: str, ip_adress: str, port: int = 12345):
+        "To be run on a separate thread. Setup connection as host and store the handle created as `self.handle`."
+        self.handle = server.host(username, ip_adress, port=port)
+        # No handler yet, don't know what you want to use.
+    
     def state_manager_hook(self, app):
         if len(State.state_tree) >= 4:
             if State.state_tree[3] == self.local_options[1]:
