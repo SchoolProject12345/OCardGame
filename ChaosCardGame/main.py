9866171c91fb2a8d46c07984ff5deb3d2e7001c0
AxaQuilPre <tablemaker99@gmail.com>
2024-02-22 11:02:07 +0100

    Wrapped main into a function. (#45)

--- ChaosCardGame/main.py
+++ ChaosCardGame/main.py
@@ -6,11 +6,15 @@ setup_logger()
 from UserInterface.ocg_app import OcgGame
 #fmt: on
 
-if __name__ == "__main__":
+
+def main():
     # try:
     logging.info("Launching app")
     app = OcgGame()
     app.start()
+
     # except Exception as e:
     #     logging.critical(f"Unhandeled exception: {e}")
     #     traceback.print_exc()
+if __name__ == "__main__":
+    main()
