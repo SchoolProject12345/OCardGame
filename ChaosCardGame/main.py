f4dff3b84e9005bec624c7cd9b46d70e2cd5621f
AxaQuilPre <tablemaker99@gmail.com>
2024-02-28 07:02:14 +0100

    Refactor

--- ChaosCardGame/main.py
+++ ChaosCardGame/main.py
@@ -11,14 +11,10 @@ from UserInterface.ocg_app import OcgGame
 from utility import get_setting
 
 def main():
-    # try:
-        logging.info("Launching app")
-        app = OcgGame()
-        app.start()
+    logging.info("Launching app")
+    app = OcgGame()
+    app.start()
 
-    # except Exception as e:
-        # logging.critical(f"Unhandeled exception: {e}")
-        # traceback.print_exc()
 if __name__ == "__main__":
     # try:
     main()
