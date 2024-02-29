feb127f7944355c24e905e687ddb042a88317113
ErdaBerda <66795312+ErdaBerda@users.noreply.github.com>
2024-02-29 16:00:43 +0100

    Revert "asdf"
    This reverts commit f50f54e3b740fb67529480a43c10b85deed33bdb.

--- spammer.py
+++ spammer.py
@@ -1,11 +1,11 @@
 import os
 
 while True:
-    os.system("touch spamfile.txt")
+    os.system("mkdir spamdir")
     os.system("git add .")
     os.system("git commit -m 'asdf'")
     os.system("git push")
-    os.system("rm spamfile.txt")
+    os.system("rm -r spamdir")
     os.system("git add .")
     os.system("git commit -m 'fdsa'")
     os.system("git push")
