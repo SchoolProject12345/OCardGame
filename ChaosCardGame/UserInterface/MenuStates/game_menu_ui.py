cc7252db397f2d73b046526e2be3aae54b4dc244
DrN1ghtW0lfe <leonard.bijlenga@gmail.com>
2024-02-29 12:03:04 +0100

    commented sound_handle (more like sound_crasher)

--- ChaosCardGame/UserInterface/MenuStates/game_menu_ui.py
+++ ChaosCardGame/UserInterface/MenuStates/game_menu_ui.py
@@ -321,7 +321,7 @@ class GameMenu(State):
             )
             # leaving print for now, remove after testing
 
-            sound_handle("fire", "play", channel=8)
+            #sound_handle("fire", "play", channel=8)
 
             user_slot = self.pending_actions[0].slot
             target_slot = self.pending_actions[1].slot
@@ -336,7 +336,7 @@ class GameMenu(State):
                 f"CARD_ATTACK, From: {self.pending_actions[0].slot} to {self.pending_actions[1].slot} with attack: {self.pending_actions[0].attack}"
             )
 
-            sound_handle("fire", "play", channel=8)
+            #sound_handle("fire", "play", channel=8)
 
             user_slot = self.pending_actions[0].slot
             target_slot = self.pending_actions[1].slot
@@ -355,7 +355,7 @@ class GameMenu(State):
                 print(
                     f"ULTIMATE, From: {self.pending_actions[0].slot} to {self.pending_actions[1].slot} with attack: {self.pending_actions[0].attack}"
                 )
-            sound_handle("ultimateattack", "play", channel=8)
+            #sound_handle("ultimateattack", "play", channel=8)
             target_slot = self.pending_actions[1].slot
             handle.run_action(f"attack|ally@|1|{slottuple2index(target_slot)}")
         elif (
