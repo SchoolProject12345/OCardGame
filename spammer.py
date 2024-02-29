0830b46a587c84341725a3f9e7ee5687902edd71
Ornit <ornithopter944@gmail.com>
2024-02-29 09:50:25 +0100

    asdf

--- spammer.py
+++ spammer.py
@@ -3,9 +3,9 @@ import os
 while True:
     os.system("touch spamfile.txt")
     os.system("git add .")
-    os.system("git commit -m 'asdf'")
+    os.system("git commit -m 'among us is the game'")
     os.system("git push")
     os.system("rm spamfile.txt")
     os.system("git add .")
-    os.system("git commit -m 'fdsa'")
+    os.system("git commit -m 'amolgongalous'")
     os.system("git push")
