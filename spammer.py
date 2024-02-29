e755bf7a79abbb92fc55011fd76e6ce144e8c59c
ErdaBerda <66795312+ErdaBerda@users.noreply.github.com>
2024-02-29 16:00:43 +0100

    Revert "amolgongalous"
    This reverts commit 67539afa27620e85065f78030ceac353d07a8561.

--- spammer.py
+++ spammer.py
@@ -3,9 +3,9 @@ import os
 while True:
     os.system("touch spamfile.txt")
     os.system("git add .")
-    os.system("git commit -m 'among'")
+    os.system("git commit -m 'among us is the game'")
     os.system("git push")
     os.system("rm spamfile.txt")
     os.system("git add .")
-    os.system("git commit -m 'us'")
+    os.system("git commit -m 'amolgongalous'")
     os.system("git push")
