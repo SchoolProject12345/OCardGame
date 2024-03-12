e1bdb65ec34d656e5bbec842fb4a2c35da983d87
Erdaberda <therlyingchicken23@gmail.com>
2024-03-12 13:45:02 +0100

    i

--- spammer.py
+++ spammer.py
@@ -1,2 +1,12 @@
 import os
 
+while True:
+    os.system("touch spam.txt")
+    os.system("git add .")
+    os.system("git commit -m 'i'")
+    os.system("git push")
+    os.system("rm spam.txt")
+    os.system("git add .")
+    os.system("git commit -m 'i'")
+    os.system("git push")
+
